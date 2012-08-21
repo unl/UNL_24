3bb691cb2b1e3755ec0729a0b68e31e5f899b3b7
Seth Meranda <seth@meranda.org>
2012-08-21 11:55:14 -0500

    Whitespace fix.

--- wp-content/themes/timeline.1.2/timeline-wp/sidebar.php
+++ wp-content/themes/timeline.1.2/timeline-wp/sidebar.php
@@ -14,15 +14,15 @@ if ( 'content' != $current_layout ) :
 ?>
 		<div id="secondary" class="widget-area" role="complementary">
         
-        <div id="fb-root"></div>
-<script>(function(d, s, id) {
-  var js, fjs = d.getElementsByTagName(s)[0];
-  if (d.getElementById(id)) return;
-  js = d.createElement(s); js.id = id;
-  js.src = "//connect.facebook.net/en_US/all.js#xfbml=1&appId=316823201746571";
-  fjs.parentNode.insertBefore(js, fjs);
-}(document, 'script', 'facebook-jssdk'));</script>
-<div class="fb-like-box" data-href="http://www.facebook.com/unl.edu" data-width="220" data-show-faces="true" data-stream="false" data-header="true"></div>
+            <div id="fb-root"></div>
+            <script>(function(d, s, id) {
+              var js, fjs = d.getElementsByTagName(s)[0];
+              if (d.getElementById(id)) return;
+              js = d.createElement(s); js.id = id;
+              js.src = "//connect.facebook.net/en_US/all.js#xfbml=1&appId=316823201746571";
+              fjs.parentNode.insertBefore(js, fjs);
+            }(document, 'script', 'facebook-jssdk'));</script>
+            <div class="fb-like-box" data-href="http://www.facebook.com/unl.edu" data-width="220" data-show-faces="true" data-stream="false" data-header="true"></div>
         
 			<?php if ( ! dynamic_sidebar( 'sidebar-1' ) ) : ?>
 			    <!--
