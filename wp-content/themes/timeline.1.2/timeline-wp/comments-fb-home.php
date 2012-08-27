21ccc544eeb667999147f54a551c96fabd569f30
Seth Meranda <seth@meranda.org>
2012-08-27 11:47:51 -0500

    Slim down FB region for testing purposes.

--- wp-content/themes/timeline.1.2/timeline-wp/comments-fb-home.php
+++ wp-content/themes/timeline.1.2/timeline-wp/comments-fb-home.php
@@ -7,8 +7,8 @@
   fjs.parentNode.insertBefore(js, fjs);
 }(document, 'script', 'facebook-jssdk'));</script>
 
-<fb:like send="true" width="440" show_faces="false" font="lucida grande" href="<?php the_permalink(); ?>"></fb:like>
+<fb:like send="true" width="300" show_faces="false" font="lucida grande" href="<?php the_permalink(); ?>"></fb:like>
 
-<fb:comments href="<?php the_permalink(); ?>" num_posts="1" width="440"></fb:comments>
+<fb:comments href="<?php the_permalink(); ?>" num_posts="1" width="300"></fb:comments>
 
 
