66592a9c81bdb3cdafc8a9f42f8b66a978110f52
Seth Meranda <seth@meranda.org>
2012-08-27 11:59:01 -0500

    upgrade facebook comments to fluid style. #2.

--- wp-content/themes/timeline.1.2/timeline-wp/comments-fb-home.php
+++ wp-content/themes/timeline.1.2/timeline-wp/comments-fb-home.php
@@ -1,14 +1,14 @@
- <div id="fb-root"></div>
+<div id="fb-root"></div>
 <script>(function(d, s, id) {
   var js, fjs = d.getElementsByTagName(s)[0];
-  if (d.getElementById(id)) {return;}
+  if (d.getElementById(id)) return;
   js = d.createElement(s); js.id = id;
-  js.src = "//connect.facebook.net/en_US/all.js#xfbml=1";
+  js.src = "//connect.facebook.net/en_US/all.js#xfbml=1&appId=316823201746571";
   fjs.parentNode.insertBefore(js, fjs);
 }(document, 'script', 'facebook-jssdk'));</script>
 
-<fb:like send="true" width="300" show_faces="false" font="lucida grande" href="<?php the_permalink(); ?>"></fb:like>
+<div class="fb-like" data-href="<?php the_permalink(); ?>" data-send="true" data-width="400" data-show-faces="false"></div>
 
-<fb:comments href="<?php the_permalink(); ?>" num_posts="1" width="300"></fb:comments>
+<div class="fb-comments" data-href="<?php the_permalink(); ?>" data-num-posts="1" data-width="400"></div>
 
 
