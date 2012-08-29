ffcab929b8b9deadb8baf7adccc4ba00e397bbe0
s-cwiedel5 <cwiedel@cse.unl.edu>
2012-08-29 11:26:52 -0500

    Modified commit Styles in css, replaced ajax loader image, no more scoped css

--- wp-content/plugins/infinite-scroll/PresetDB.csv.php
+++ wp-content/plugins/infinite-scroll/PresetDB.csv.php
@@ -4,4 +4,5 @@ inove,#main,"#main div.post",#pagenavi,"#pagenavi span.older a:first"
 "twenty ten",#content,div.navigation,"#content  div.post","div.navigation a:first"
 "twenty eleven",#content,#nav-below,"#content article.post","#nav-below .nav-previous a:first"
 atahualpa,td#middle,div.navigation-bottom,"td#middle div.post","div.newer a:link"
+"UNL24 Timeline",#content,#nav-below,"#content  article.post","#nav-below .nav-previous a:first"
 End Preset DB*/ ?>
--- wp-content/plugins/twitter-blackbird-pie/blackbird-pie.php
+++ wp-content/plugins/twitter-blackbird-pie/blackbird-pie.php
@@ -306,33 +306,7 @@ class BlackbirdPie {
         $tweet = $tweet_details['tweet_text'];
 		
         $tweetHTML = "<!-- tweet id : $id --> 
-        <style type='text/css'>
-            #bbpBox_$id a { text-decoration:none; }
-            #bbpBox_$id a:hover { text-decoration:underline; }
-			#bbpBox_$id.bbpBox > div {
-				background:#fff;
-				padding:10px;
-				margin:0;
-				min-height:48px;
-				-moz-border-radius:5px;
-				-webkit-border-radius:5px;
-				border-radius:5px;
-				border: 1px dashed #D5D5D5;
-				-moz-box-shadow: 0 0 0 5px white, 1px 1px 12px 0 rgba(0, 0, 0, 0.85);
-				-webkit-box-shadow: 0 0 0 5px white, 1px 1px 12px 0 rgba(0, 0, 0, 0.85);
-				box-shadow: 0 0 0 5px white, 1px 1px 12px 0 rgba(0, 0, 0, 0.85);
-				-webkit-transition: all 0.55s ease-in-out;
-    			-moz-transition: all 0.55s ease-in-out;
-    			-o-transition: all 0.55s ease-in-out;
-    			-ms-transition: all 0.55s ease-in-out;
-    			transition: all 0.55s ease-in-out; }
-			#bbpBox_$id.bbpBox > div:hover {
-				-moz-box-shadow: 0 0 0 5px white, 0px 0px 25px 10px rgba(60, 185, 224, 0.65);
-				-webkit-box-shadow: 0 0 0 5px white, 0px 0px 25px 10px rgba(60, 185, 224, 0.65);
-				box-shadow: 0 0 0 5px white, 0px 0px 25px 10px rgba(60, 185, 224, 0.65);
-			}
-        </style>
-		
+       	
         <div id='bbpBox_$id' class='bbpBox' style='margin-bottom:20px;'>
             <div>
                 <span style='width:100%; font-size:18px; line-height:22px;'>
@@ -344,7 +318,7 @@ class BlackbirdPie {
                 </div>
                 <div style='float:left; padding:0; margin:0'>
                     <a href='{$profile_url}'>
-                        <img style='width:48px; height:48px; padding-right:7px; border:none; background:none; margin:0' src='{$profile_pic}' />
+                        <img style='-moz-box-shadow:none; -webkit-box-shadow:none; box-shadow:none; -moz-border-radius:0; -webkit-border-radius:0; border-radius:0; width:48px; height:48px; padding-right:7px; border:none; background:none; margin:0' src='{$profile_pic}' />
                     </a>
                 </div>
                 <div style='float:left; padding:0; margin:0'>
