7e42e35cc6eb730a0e1d3e49238171e12278d4e4
Seth Meranda <seth@meranda.org>
2012-09-17 14:37:52 -0500

    Add basic structure for splash page theme.

---
+++ wp-content/themes/24splash/index.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * @package WordPress
+ * @subpackage 24splash_Theme
+ */
+?>
+<html>
+    <head>
+        <title>UNL24</title>
+    </head>
+    <body>
+        This is the splash page
+    </body>
+</html>
